a444c7a2b943eb60db4c05a4dfb1f884d87c9377
nolood <a9514548799@gmail.com>
2023-10-10 16:30:07 +0500

    feat: types for effects

--- src/entities/auth/lib/auth-effects.ts
+++ src/entities/auth/lib/auth-effects.ts
@@ -1,33 +1,39 @@
-import { api } from "@/shared/api/api";
-import { createEffect } from "effector";
-import { setToken } from "@/shared/lib";
+import { api } from '@/shared/api/api'
+import { setToken } from '@/shared/lib'
+import { createEffect } from 'effector'
 
-import { User } from "@/entities/user/model/user";
+import { User } from '@/entities/user/model/user'
 
-export const registerFx = createEffect<void, User, Error>(
-  async (params: User) => {
-    try {
-      const { data } = await api.post("/auth/registration", {
-        ...params,
-      });
-      setToken(data.token);
-      return data;
-    } catch (error) {
-      console.log(error);
-      return error;
-    }
-  }
-);
+export const registerFx = createEffect<
+	{ username: string; email: string; password: string },
+	User,
+	Error
+>(async (params: User) => {
+	try {
+		const { data } = await api.post('/auth/registration', {
+			...params,
+		})
+		setToken(data.token)
+		return data
+	} catch (error) {
+		console.log(error)
+		return error
+	}
+})
 
-export const loginFx = createEffect<void, User, Error>(async (params) => {
-  try {
-    const { data } = await api.post("/auth/login", {
-      ...params,
-    });
-    setToken(data.token);
-    return data;
-  } catch (error) {
-    console.log(error);
-    return error;
-  }
-});
+export const loginFx = createEffect<
+	{ username: string; email: string; password: string },
+	User,
+	Error
+>(async params => {
+	try {
+		const { data } = await api.post('/auth/login', {
+			...params,
+		})
+		setToken(data.token)
+		return data
+	} catch (error) {
+		console.log(error)
+		return error
+	}
+})
--- src/entities/user/model/user.ts
+++ src/entities/user/model/user.ts
@@ -1,24 +1,23 @@
-import { createStore } from "effector/effector.mjs";
-import { loginFx, registerFx } from "@/entities/auth/lib";
-import { getSelfFx } from "@/entities/user/lib";
-import { AxiosError } from "axios";
+import { loginFx, registerFx } from '@/entities/auth/lib'
+import { getSelfFx } from '@/entities/user/lib'
+import { createStore } from 'effector/effector.mjs'
 
 export type User = {
-  username: string;
-  email?: string;
-  password: string;
-};
+	username: string
+	email?: string
+	password: string
+}
 
 export const $user = createStore<User | null>(null)
-  .on(loginFx.doneData, (_, user) => user)
-  .on(loginFx.failData, (_, error) => {
-    console.log(error);
-  })
-  .on(registerFx.doneData, (_, user) => user)
-  .on(registerFx.failData, (_, error) => {
-    console.log(error);
-  })
-  .on(getSelfFx.doneData, (_, user) => user)
-  .on(getSelfFx.failData, (_, error) => {
-    console.log(error);
-  });
+	.on(loginFx.doneData, (_, user) => user)
+	.on(loginFx.failData, (_, error) => {
+		console.log(error)
+	})
+	.on(registerFx.doneData, (_, user) => user)
+	.on(registerFx.failData, (_, error) => {
+		console.log(error)
+	})
+	.on(getSelfFx.doneData, (_, user) => user)
+	.on(getSelfFx.failData, (_, error) => {
+		console.log(error)
+	})
